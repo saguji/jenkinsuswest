a05f5269a51df96aebfbfaf2a070e1d033fe9349
saurabh gupta <saurabhmlgupta84@gmail.com>
2018-05-08 17:43:38 +0530

    8may18 change healthy percent

--- www/index.php
+++ www/index.php
@@ -17,7 +17,7 @@
 </head>
 <body>
 	<img id="logo" src="logo.png" />
-	<h1><?php echo "Hello".($_ENV["NAME"]?$_ENV["NAME"]:" Saurabh! Trying on 08-May-18?")."!"; ?></h1>
+	<h1><?php echo "Hello".($_ENV["NAME"]?$_ENV["NAME"]:" Saurabh! 08-May-18. After changin healy percent")."!"; ?></h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
